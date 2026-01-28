ebc343d413da994c704ce95778fd4a673e732c10
asifazad <asifazad114@gmail.com>
2026-01-28 11:24:47 +0530

    error fixed

--- server/config/mongoDb.js
+++ server/config/mongoDb.js
@@ -5,7 +5,7 @@ async function mongoDb(url) {
     try {
         console.log(url);
 
-        await mongoose.connect('mongodb+srv://hasifazad:5BdRtnQajKLA7pBT@cluster0.9i6hcim.mongodb.net/mythra');
+        await mongoose.connect(url);
         console.log("mongoDb is Connected");
     } catch (err) {
         console.error("Connection Error", err);
